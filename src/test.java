042437edbd3b6c5ffd5e7378004be93cfc505651
宇sir <1214706258@qq.com>
2020-04-23 13:59:08 +0800

    diyici

---
+++ src/test.java
@@ -0,0 +1,11 @@
+/**
+ * ceshi
+ *
+ * @Author: sjy
+ * @Date: 2020/4/23 13:54
+ */
+public class test {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
